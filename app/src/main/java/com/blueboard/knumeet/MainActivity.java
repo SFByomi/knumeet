12d2211078f19031ab0f04b125947591ae3047be
junu0124 <51398506+junu0124@users.noreply.github.com>
2020-09-26 19:27:00 +0900

    commit test

--- app/src/main/java/com/blueboard/knumeet/MainActivity.java
+++ app/src/main/java/com/blueboard/knumeet/MainActivity.java
@@ -3,12 +3,16 @@ package com.blueboard.knumeet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView tv = new TextView(this);
+        tv.setText("test");
+
+        setContentView(tv);
     }
 }
