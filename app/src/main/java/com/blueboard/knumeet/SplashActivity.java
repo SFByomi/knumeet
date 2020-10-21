45db27dfe93c4e2f3ce4751b4168b9085821eda3
yomin <yomin2627@gmail.com>
2020-10-21 22:48:24 +0900

    구글api

--- app/src/main/java/com/blueboard/knumeet/SplashActivity.java
+++ app/src/main/java/com/blueboard/knumeet/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         try {
             Thread.sleep(2000); //대기 초 설정
-            startActivity(new Intent(SplashActivity.this, LoginActivity.class));//이미지 후 메인액티비티
+            startActivity(new Intent(SplashActivity.this, LoginActivity.class));//이미지 후 로그인액티비티
 
             finish();
         } catch (Exception e) {
