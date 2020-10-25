813748bf6aacb81fc9dbf4c95fafb7002b32492c
yomin <yomin2627@gmail.com>
2020-10-25 21:52:32 +0900

    src 변경

--- app/src/main/java/com/blueboard/knumeet/PopupActivity.java
+++ app/src/main/java/com/blueboard/knumeet/PopupActivity.java
@@ -7,6 +7,13 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 
+import com.wisnu.datetimerangepickerandroid.CalendarPickerView;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import static com.wisnu.datetimerangepickerandroid.CalendarPickerView.SelectionMode.RANGE;
+
 public class PopupActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,11 +21,20 @@ public class PopupActivity extends Activity {
         //타이틀바 없애기
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.popupactivity);
+
+        Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 1);
+
+        CalendarPickerView calendar = (CalendarPickerView) findViewById(R.id.calendar_view);
+        Date today = new Date();
+        calendar.init(today, nextYear.getTime())
+                .inMode(RANGE);
     }
 
     //확인 버튼 클릭
     public void MakingOnClick(View v){
         //데이터 전달하기
+
         Intent intent = new Intent();
         intent.putExtra("result", "Room!");
         setResult(RESULT_OK, intent);
