64d6b9d79bd92fc2b8245f2b3a6747eb0f3158a1
shinhj7164 <71446664+shinhj7164@users.noreply.github.com>
2020-10-27 05:22:09 +0900

    2 recycler views

---
+++ app/src/main/java/com/blueboard/knumeet/Adapter_times.java
@@ -0,0 +1,70 @@
+package com.blueboard.knumeet;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Adapter_times extends RecyclerView.Adapter<Adapter_times.CustomViewHolder> {
+
+    private int[] arr_times = new int [24];
+    private Context context;
+
+    public Adapter_times(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Adapter_times.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_times,parent,false);
+        CustomViewHolder holder = new CustomViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter_times.CustomViewHolder holder, int position) {
+       holder.tv_times.setText(position +"~" +  (position + 1));
+    }
+
+    @Override
+    public int getItemCount() {
+        return (24);
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_times;
+
+        public CustomViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            this.tv_times = itemView.findViewById(R.id.tv_times);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("ResourceAsColor")
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+                        if(arr_times[pos] == 0) {
+                            view.setBackgroundColor(Color.GREEN);
+                            arr_times[pos] = 1;
+                        }
+                        else
+                        {
+                            view.setBackgroundColor(Color.WHITE);
+                            arr_times[pos] = 0;
+                        }
+                    }
+                }
+            });
+
+        }
+    }
+}
--- app/src/main/java/com/blueboard/knumeet/MakeInfoActivity.java
+++ app/src/main/java/com/blueboard/knumeet/MakeInfoActivity.java
@@ -1,6 +1,8 @@
 package com.blueboard.knumeet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,10 +12,38 @@ import android.widget.ImageButton;
 
 public class MakeInfoActivity extends AppCompatActivity {
     ImageButton btn_back_info_to;
+
+    private RecyclerView recyclerView_days,recyclerView_times;
+    private RecyclerView.Adapter adapter_days,adapter_times;
+    private RecyclerView.LayoutManager layoutManager_days,layoutManager_times;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_info);
+
+        recyclerView_days = findViewById(R.id.rv_days);//d
+
+        recyclerView_times = findViewById(R.id.rv_times);//t
+
+        layoutManager_days = new LinearLayoutManager(this);
+        layoutManager_times = new LinearLayoutManager(this);
+
+        recyclerView_days.setLayoutManager(layoutManager_days);//d
+
+        recyclerView_times.setLayoutManager(layoutManager_times);//t
+
+        adapter_days = new Adapter_Days(this);//d
+
+        recyclerView_days.setAdapter(adapter_days);//어뎁터 연결//d
+
+        adapter_times = new Adapter_times(this);//t
+
+        recyclerView_times.setAdapter(adapter_times);//t
+
+
+
 
         btn_back_info_to = (ImageButton)findViewById(R.id.btn_back_info_to);
 
@@ -23,6 +53,9 @@ public class MakeInfoActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+
 
     }
 }
