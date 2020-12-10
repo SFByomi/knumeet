92e65e5690935110c5a110ee098bdaff7ee389a9
shinhj7164 <71446664+shinhj7164@users.noreply.github.com>
2020-12-11 02:23:49 +0900

    12/11

--- app/src/main/java/com/blueboard/knumeet/CompleteActivity.java
+++ app/src/main/java/com/blueboard/knumeet/CompleteActivity.java
@@ -3,11 +3,12 @@ package com.blueboard.knumeet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-import static maes.tech.intentanim.CustomIntent.customType;
 
 public class CompleteActivity extends AppCompatActivity {
 
@@ -15,7 +16,6 @@ public class CompleteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete);
-        customType(CompleteActivity.this,"bottom-to-up");//시작될때 animation
         Button btn_go_home = (Button)findViewById(R.id.btn_go_home);
         btn_go_home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +24,22 @@ public class CompleteActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        ImageView image_share = findViewById(R.id.image_share);
+        image_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Sharing_intent = new Intent(Intent.ACTION_SEND);
+                Sharing_intent.setType("text/plain");
+
+                String Test_Message = "-MNrOk8lJqRA1mHmON-Z";// 멘트
+
+                Sharing_intent.putExtra(Intent.EXTRA_TEXT, Test_Message);
+
+                Intent Sharing = Intent.createChooser(Sharing_intent, "공유하기");
+                startActivity(Sharing);
+            }
+        });
+
     }
 }
